582d69bf3f616b713660568a0cd95a150e2a2247
damian <adamodamian6@gmail.com>
2023-04-26 14:30:54 -0300

    carrito de compra conectado al front

--- src/main/java/com/damian/backend/cardapp/backendcardapp/controlador/ProductoControlador.java
+++ src/main/java/com/damian/backend/cardapp/backendcardapp/controlador/ProductoControlador.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.damian.backend.cardapp.backendcardapp.entidad.Producto;
 import com.damian.backend.cardapp.backendcardapp.services.ProductoService;
-
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/productos")
 public class ProductoControlador {
